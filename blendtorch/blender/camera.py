4c1dbb684a549ad75d83ca203781f91b72e57fe1
Christoph Heindl <christoph.heindl@gmail.com>
2020-07-03 17:19:25 +0200

    adds unused function

--- blendtorch/blender/camera.py
+++ blendtorch/blender/camera.py
@@ -16,3 +16,30 @@ def view_projection_matrix(camera=None):
     view_matrix = camera.matrix_world.inverted()
     proj_matrix = camera.calc_matrix_camera(bpy.context.evaluated_depsgraph_get(), x=shape[1], y=shape[0])
     return view_matrix, proj_matrix
+
+def project_points(obj, camera=None):
+    # https://docs.blender.org/api/current/bpy_extras.object_utils.html
+    # https://github.com/dfelinto/blender/blob/master/release/scripts/modules/bpy_extras/object_utils.py
+    '''Get 2D projection coordinates object's vertex coordinates.'''
+    camera = camera or bpy.context.scene.camera
+    scene = bpy.context.scene
+    data = obj.data
+    mat = obj.matrix_world
+
+    verts = data.vertices
+    if ids is not None:
+        verts = [data.vertices[i] for i in ids]
+
+    xyz = []
+    for v in verts:
+        p = bpy_extras.object_utils.world_to_camera_view(scene, camera, mat @ v.co)
+        xyz.append(p) # Normalized 2D in W*RS / H*RS
+
+    xyz = np.stack(xyz).astype(np.float32)
+    xyz[...,1] = 1. - xyz[...,1] # Blender has origin bottom-left.
+    return xyz # normalized xy but unnormalized in z
+
+def normalized_keypoints(obj):
+    xyz = get_pixels(obj, ids=obj['keypoints'])    
+    mask = np.logical_and((xyz > 0).all(1), (xyz[:, :2] < 1).all(1))       
+    return xyz, mask
