c175b870e304173155f7c652099b3178efa3394d
Heindl Christoph <cheind@profactor.at>
2020-07-09 11:36:55 +0200

    added docs

--- pkg_blender/blendtorch/btb/signal.py
+++ pkg_blender/blendtorch/btb/signal.py
@@ -1,29 +1,42 @@
 from functools import partial
 
 class Signal:
+    '''Distribute messages to multiple callbacks.'''
     def __init__(self):
         self.slots = []
 
     def add(self, fn, *args, **kwargs):
+        '''Register `fn` as callback.
+        
+        Params
+        ------
+        *args: optional
+            Additional positional arguments to provide callback with
+        **kwargs: optional
+            Additional keyword arguments to provide callback with
+
+        Returns
+        -------
+        handle: object
+            Handle that can be used to unregister callback.
+        '''
         fnp = partial(fn, *args, **kwargs)        
         self.slots.append(fnp)
         return fnp
 
     def remove(self, handle):
+        '''Unregister callback using handle returned from `add`.'''
         self.slots.remove(handle)
 
     def invoke(self, *args, **kwargs):
+        '''Invoke the signal.
+
+        Params
+        ------
+        *args: optional
+            Positional arguments to send to all callbacks
+        **kwargs: optional
+            Keyword arguments to send to all callbacks
+        '''
         for s in self.slots:
             s(*args, **kwargs)
-
-
-if __name__ == '__main__':
-    s = Signal()
-
-    def printme(arg, arg2, kw=None):
-        print(arg, arg2, kw)
-
-    s.add(printme, arg2=10, kw='hello')
-    s.add(printme, arg2=5, kw='wuff')
-
-    s('common')
