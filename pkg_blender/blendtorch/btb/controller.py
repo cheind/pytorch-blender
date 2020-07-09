bc49d6fd60cad98ee1cca2d93a70857484f8daec
Heindl Christoph <cheind@profactor.at>
2020-07-09 11:48:49 +0200

    fix post_animation

--- pkg_blender/blendtorch/btb/controller.py
+++ pkg_blender/blendtorch/btb/controller.py
@@ -4,16 +4,25 @@ import bpy
 from .signal import Signal
 
 class Controller:
-    '''Animation controller with fine-grained callbacks.'''
+    '''Animation controller with fine-grained callbacks.
+    
+    Exposes the following signals
+     - pre_animation() invoked before animation starts
+     - pre_frame() invoked before a frame begins
+     - post_frame() invoked after a frame is finished
+     - post_animation() invoked after an animation completed
+    '''
     
     def __init__(self):
         self.pre_animation = Signal()
         self.pre_frame = Signal()
+        self.post_frame = Signal()
         self.post_animation = Signal()
         self.is_playing = False
         self.h_pre_frame = bpy.app.handlers.frame_change_pre.append(self._on_pre_frame)
+        self.h_post_frame = bpy.app.handlers.frame_change_post.append(self._on_post_frame)
         
-    def play(self, once=True, startframe=None, endframe=None):
+    def play(self, once=False, startframe=None, endframe=None):
         self._set_frame_range(startframe, endframe)
         self.once = once
         self.is_playing = True        
@@ -32,13 +41,21 @@ class Controller:
 
         cur = bpy.context.scene.frame_current
         pre_first = (cur == bpy.context.scene.frame_start)
-        post_last = (bpy.context.scene.frame_current == bpy.context.scene.frame_end + 1)
+        
+        if pre_first:
+            self.pre_animation.invoke()
+        self.pre_frame.invoke()
 
-        if self.once and post_last:
-            bpy.ops.screen.animation_cancel()
-            self.is_playing = False
+    def _on_post_frame(self, scene, *args):
+        if not self.is_playing:
+            return
+
+        cur = bpy.context.scene.frame_current
+        post_last = (cur == bpy.context.scene.frame_end)
+
+        if post_last:
+            if self.once:
+                bpy.ops.screen.animation_cancel()
+                self.is_playing = False
             self.post_animation.invoke()
-        else:
-            if pre_first:
-                self.pre_animation.invoke()
-            self.pre_frame.invoke()
+        self.post_frame.invoke()
