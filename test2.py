130bb07664fdfc9f052b39d34b2754816efdb76b
Heindl Christoph <cheind@profactor.at>
2020-07-20 16:55:26 +0200

    cleanup

--- test2.py
+++ test2.py
@@ -140,3 +140,17 @@ off.proj_matrix = btb.camera.projection_matrix()
 
 env = btb.gym.BaseEnv()
 env.reset()
+
+
+# import zmq
+
+# #  Prepare our context and sockets
+# context = zmq.Context()
+# socket = context.socket(zmq.REQ)
+# socket.connect("tcp://localhost:5559")
+
+# #  Do 10 requests, waiting each time for a response
+# for request in range(1,11):
+#     socket.send(b"Hello")
+#     message = socket.recv()
+#     print("Received reply %s [%s]" % (request, message))
