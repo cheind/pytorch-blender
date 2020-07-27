1f54436d11056eae0e0e890b13d17f27e3829176
Christoph Heindl <christoph.heindl@gmail.com>
2020-07-27 16:44:16 +0200

    added docs

--- pkg_blender/blendtorch/btb/arguments.py
+++ pkg_blender/blendtorch/btb/arguments.py
@@ -3,7 +3,29 @@ import argparse
 import os
 
 def parse_blendtorch_args(argv=None):
-    '''Parses blendtorch instance parameters and returns the remainder arguments.'''    
+    '''Parses blendtorch instance parameters and returns the remainder arguments.
+    
+    This method is intended to be used with Blender instances launched via 
+    `btt.BlenderLauncher`. It parses specific command line arguments that
+     - identify the Blender process `btid`,
+     - provide a random number seed `btseed`, and
+     - lists a number of named socket addresses to connect to.
+
+    This script parses command-line arguments after a special end of
+    command line element `--`.
+    
+    Params
+    ------
+    argv: list-like, None
+        The command line arguments to be parsed.
+
+    Returns
+    -------
+    args: argparse.Namespace
+        The parsed command line arguments
+    remainder: list-like
+        The remaining command line arguments.
+    '''    
     argv = argv or sys.argv
     if '--' in argv:
         argv = argv[argv.index("--") + 1:]
