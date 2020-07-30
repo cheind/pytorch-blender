0bb152fa5ad33689b25bbedf71e44d69c69a0483
Christoph Heindl <christoph.heindl@gmail.com>
2020-07-30 18:27:24 +0200

    beginning of travis.ci files

---
+++ scripts/install_blender.sh
@@ -0,0 +1,16 @@
+set -e
+
+NAME="blender-2.83.3-linux64"
+NAMETAR="${NAME}.tar.xz"
+CACHE="${HOME}/.blender-cache"
+TAR="${CACHE}/${NAMETAR}"
+URL="http://mirror.cs.umn.edu/blender.org/release/Blender2.83/${NAMETAR}"
+
+echo "Installing Blender ${NAME}"
+mkdir -p $CACHE
+if [ ! -f $TAR ]; then
+    wget -O $TAR $URL
+fi
+tar -xjf $TAR -C $HOME
+
+echo "export BLENDER_BIN=\"${HOME}/${NAME}/blender\"" > .envs
