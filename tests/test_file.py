932825127d5f80c15b5ddb81185bcb5e24ead56f
Heindl Christoph <cheind@profactor.at>
2020-07-30 10:06:19 +0200

    renamed

--- tests/test_file.py
+++ tests/test_file.py
@@ -2,7 +2,7 @@ import pytest
 from blendtorch.btt.file import FileRecorder, FileReader
 
 @pytest.mark.offscreen
-def test_file_record_write(tmp_path):
+def test_file_recorder_reader(tmp_path):
     with FileRecorder(outpath=tmp_path/'record.mpkl', max_messages=10) as rec:
         for i in range(7):
             rec.save({'value':i}, is_pickled=False)
