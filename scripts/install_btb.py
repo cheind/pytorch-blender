708fd6ff74bcb33e2d12916ead72979c0bca497f
Christoph Heindl <christoph.heindl@gmail.com>
2020-07-31 13:52:16 +0200

    bootstrap pip

--- scripts/install_btb.py
+++ scripts/install_btb.py
@@ -11,24 +11,32 @@ from pathlib import Path
 
 THISDIR = Path(__file__).parent
 
-def install(name, upgrade=True, user=True, editable=False):
+def run(cmd):
     try:
-        cmd = [bpy.app.binary_path_python, '-m', 'pip', 'install']
-        if upgrade:
-            cmd.append('--upgrade')
-        if user:
-            cmd.append('--user')
-        if editable:
-            cmd.append('-e')
-        cmd.append(name)
         output = subprocess.check_output(cmd)
         print(output)
     except subprocess.CalledProcessError as e:
         print(e.output)
         sys.exit(1)
 
+def install(name, upgrade=True, user=True, editable=False):
+    cmd = [bpy.app.binary_path_python, '-m', 'pip', 'install']
+    if upgrade:
+        cmd.append('--upgrade')
+    if user:
+        cmd.append('--user')
+    if editable:
+        cmd.append('-e')
+    run(cmd)
+
+def bootstrap(user=True):
+    cmd = [bpy.app.binary_path_python, '-m', 'ensurepip', '--upgrade']
+    if user:
+        cmd.append('--user')
+    run(cmd)
+        
 def main():
-    install('pip', upgrade=True)
-    install(str(THISDIR/'..'/'pkg_blender'), editable=True)
+    bootstrap(user=True)
+    install(str(THISDIR/'..'/'pkg_blender'), editable=True, user=True)
 
 main()
