5a2609ab347c02f3aaa6078b8d36cc831b0a32dd
Heindl Christoph <cheind@profactor.at>
2020-08-04 09:09:34 +0200

    Added documentation

--- examples/control/cartpole.py
+++ examples/control/cartpole.py
@@ -1,13 +1,30 @@
+"""Cartpole controller that interacts with Blender environment through OpenAI.
+
+Run
+    python cartpole.py
+from this directory.
+
+Note, the custom environment is registered in `cartpole_gym/__init__.py`. The
+file `cartpole_gym/cartpole_env.py` defines a OpenAI compatible Blender environment.
+The actual environment logic is implemented in Blender script
+`cartpole_gym/cartpole.blend.py` and scene `cartpole_gym/cartpole.blend`.
+"""
+
+
 import gym
 import cartpole_gym
 
 KAPPA = 30
 
 def control(obs):
+    # Simple P controller defining the error as xpole-xcart
     xcart, xpole, _ = obs
     return (xpole-xcart)*KAPPA
 
 def main():
+    # Create the environment. The environment is registered in 
+    # `cartpole_gym/__init__.py`. Set `real_time=True` when you 
+    # want to keep the simulation running until the agent response. 
     env = gym.make('blendtorch-cartpole-v0', real_time=False)
     
     obs = env.reset()        
