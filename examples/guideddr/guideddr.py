ed53cefd4ed7acdc856b9a586cf43b9fe2706237
Heindl Christoph <cheind@profactor.at>
2020-08-07 11:53:42 +0200

    improves convergence by allowing a bigger standard deviation. switched to lognormal for frequencies.

--- examples/guideddr/guideddr.py
+++ examples/guideddr/guideddr.py
@@ -4,20 +4,27 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils import data
-from torch.distributions import MultivariateNormal, Normal
+from torch.distributions import MultivariateNormal, Normal, LogNormal
 from blendtorch import btt
 import torchvision.utils as vutils
+
+BATCH = 64
+REAL_LABEL = 1
+SIM_LABEL = 0
+
+LOG_MEAN_TRUE = 2.25 # rough sample range: 8.5-10.5
+LOG_STD_TRUE = 0.1
 
 def item_transform(item):
     x = item['image'].astype(np.float32)
     x = (x - 127.5) / 127.5
-    return np.transpose(x, (2, 0, 1)),  item['params'], item['mid']
+    return np.transpose(x, (2, 0, 1)),  item['params'], item['mid'], item['btid']
 
-def get_real_images(ds, duplex, m1true, m2true, n=128):
-    mid = duplex.send(m1=m1true, m2=m2true)
+def get_real_images(ds, duplex, n=128):
+    mid = duplex.send(type='lognormal', mean=[LOG_MEAN_TRUE, LOG_MEAN_TRUE], std=[LOG_STD_TRUE, LOG_STD_TRUE])
     images = []
     mids = []
-    for (img, params, rmid) in ds:
+    for (img, params, rmid, btid) in ds:
         if rmid == mid:
             images.append(img)
             mids.append(mid)
@@ -37,23 +44,27 @@ class Discriminator(nn.Module):
         ndf = 32
         nc = 3
         self.features = nn.Sequential(
-            # input is (nc) x 64 x 64
+            # input is (nc) x 128 x 128
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 32 x 32
+            # state size. (ndf) x 64 x 64
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
+            # state size. (ndf*2) x 32 x 32
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
+            # state size. (ndf*4) x 16 x 16
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 8, ndf * 16, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 16),
+            nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Conv2d(ndf * 16, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
 
@@ -69,11 +80,11 @@ def weights_init(m):
         torch.nn.init.normal_(m.weight, 1.0, 0.02)
         torch.nn.init.zeros_(m.bias)
 
-BATCH = 32
-REAL_LABEL = 1
-SIM_LABEL = 0
+
+
 
 def main():
+
     # Define how we want to launch Blender
     launch_args = dict(
         scene=Path(__file__).parent/'supershape.blend',
@@ -91,8 +102,10 @@ def main():
 
         addr = bl.launch_info.addresses['CTRL']
         duplex = btt.DuplexChannel(addr[0])
+        #duplex_uniform = btt.DuplexChannel(addr[1])
+        #duplex_uniform.send(type='uniform', low=0.0, high=20.0)
 
-        real_ds = get_real_images(sim_ds, duplex, m1true=10, m2true=10, n=BATCH*4)
+        real_ds = get_real_images(sim_ds, duplex, n=BATCH*2)
         real_dl = data.DataLoader(real_ds, batch_size=BATCH, num_workers=0, shuffle=True)
 
         dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -100,85 +113,82 @@ def main():
         netD = Discriminator().to(dev)
         netD.apply(weights_init)
 
-        sim_params = torch.tensor([7.0, 7.0], requires_grad=True)
-        last_mid = duplex.send(m1=sim_params[0].item(), m2=sim_params[1].item())
+        sim_params = torch.tensor([1.5, 1.5], requires_grad=True)
+        last_mid = duplex.send(type='lognormal', mean=sim_params.tolist(), std=[LOG_STD_TRUE*4, LOG_STD_TRUE*4])
 
-        optD = optim.Adam(netD.parameters(), lr=5e-5, betas=(0.5, 0.999))
-        optS = optim.Adam([sim_params], lr=5e-1, betas=(0.5, 0.999))
+        optD = optim.Adam(netD.parameters(), lr=1e-5, betas=(0.5, 0.999))
+        optS = optim.Adam([sim_params], lr=1e-1)
 
         gen_real = infinite_batch_generator(real_dl)
         gen_sim = infinite_batch_generator(sim_dl)
         crit = nn.BCELoss(reduction='none')
         
-        e = 0
-        b = None
+        epoch = 0
+        b = 0.
+        first = True
+        baseline = True
+        alpha = 0.98
         while True:
-            sim_img, sim_param, sim_mid = next(gen_sim)
+            
+            label = torch.full((BATCH,), REAL_LABEL, dtype=torch.float32, device=dev)
+            # Update D with real
+            netD.zero_grad()
+            real_img = next(gen_real)[0].to(dev)
+            output = netD(real_img)
+            errD_real = crit(output, label)
+            errD_real.mean().backward()
+            D_real = output.mean().item()
+
+            # Update D with sim
+            sim_img, sim_param, sim_mid, sim_btid = next(gen_sim)
+            label.fill_(SIM_LABEL)
+            output = netD(sim_img.to(dev))
+            errD_sim = crit(output, label)
+            errD_sim.mean().backward()
+            D_sim = output.mean().item()
+            if epoch % 5 == 0:
+                optD.step()
+            print('mean D real', D_real, 'mean D sim', D_sim)
+
             mask = (sim_mid == last_mid)
-            have_sim = mask.sum() > BATCH//2
-
-            if have_sim:
-                print('got data')           
-                label = torch.full((BATCH,), REAL_LABEL, dtype=torch.float32, device=dev)
-
-                # Update D with real
-                netD.zero_grad()
-                real_img = next(gen_real)[0].to(dev)
-                output = netD(real_img)
-                errD_real = crit(output, label)
-                errD_real.mean().backward()
-                D_real = output.mean().item()
-
-                # Update D with sim
-                label.fill_(SIM_LABEL)
-                output = netD(sim_img.to(dev)[mask])
-                errD_sim = crit(output, label[mask])
-                errD_sim.mean().backward()
-                D_sim = output.mean().item()
-                if e%10 == 0:
-                    optD.step()
-
-                # Update sim params
+            have_sim = mask.sum() > BATCH//4
+            if have_sim and epoch > 20:
+                # Update sim params, called only every 20epochs or so.
+                # blender generates new data meanwhile with old MID.
                 optS.zero_grad()
                 label.fill_(REAL_LABEL)
                 with torch.no_grad():
                     output = netD(sim_img.to(dev)[mask])
                     errS_sim = crit(output, label[mask])
                     GD_sim = output.mean().item()
-                
-                mv = MultivariateNormal(sim_params, torch.eye(2)*0.2)
-                sf = mv.log_prob(sim_param[mask]) * errS_sim.cpu()
-                if b != None:
-                    sf = sf - b
-                    b = 0.9 * sf.mean().detach() + (1-0.9)*b
-                else:
-                    b = sf.mean().detach()
 
+                ln0 = LogNormal(sim_params[0], LOG_STD_TRUE*4)
+                sf = ln0.log_prob(sim_param[mask][:, 0]) * (errS_sim.cpu() - b)
+                sf.mean().backward(retain_graph=True)
+
+                ln1 = LogNormal(sim_params[1], LOG_STD_TRUE*4)
+                sf = ln1.log_prob(sim_param[mask][:, 1]) * (errS_sim.cpu() - b)
                 sf.mean().backward()
 
+                if baseline:
+                    if first:
+                        b = errS_sim.mean().detach()
+                    else:
+                        b = alpha * errS_sim.mean().detach() + (1-alpha)*b
+                optS.step()                  
 
-                # mv0 = Normal(sim_params[0], 0.2)
-                # sf0 = mv0.log_prob(sim_param[mask][:, 0]) * errS_sim.detach().cpu()
-                # sf0.mean().backward()
-
-                # mv1 = Normal(sim_params[1], 0.2)
-                # sf1 = mv1.log_prob(sim_param[mask][:, 1]) * errS_sim.detach().cpu()
-                # sf1.mean().backward()
-
-                #sim_params.grad.mul_(E_sim * 10)
-                if e > 5:
-                    optS.step()
-
-                print('sim params', sim_params)
-
-                last_mid = duplex.send(m1=sim_params[0].item(), m2=sim_params[1].item())
-
-            e += 1
-            if e % 20 == 0:
+                print('sim params', sim_params, 'mean D sim', GD_sim)
+                last_mid = duplex.send(type='lognormal', mean=sim_params.tolist(), std=[LOG_STD_TRUE*4, LOG_STD_TRUE*4])        
+                first = False        
+                
+            epoch += 1
+            if epoch % 10 == 0:
                 vutils.save_image(real_img, 'tmp/real.png', normalize=True)
-                vutils.save_image(sim_img, 'tmp/sim_samples_%03d.png' % (e), normalize=True)
+                vutils.save_image(sim_img, 'tmp/sim_samples_%03d.png' % (epoch), normalize=True)
+                vutils.save_image(sim_img[sim_btid==0], 'tmp/sim_samples_mask_%03d.png' % (epoch), normalize=True)
+                
 
-            print('mean D real', D_real, 'mean D sim', D_sim)
+            
 
 if __name__ == '__main__':
     main()
--- examples/guideddr/supershape.blend.py
+++ examples/guideddr/supershape.blend.py
@@ -6,49 +6,80 @@ import sys
 sys.path.append('C:/dev/supershape')
 import supershape as sshape
 
+class RandomSupershapeSampler:
+    def __init__(self):
+        self.shape = (100, 100)        
+        self.last_sample = None
+        self.type = 'normal'
+        self.type_params = {'mean': np.array([5.,5.]), 'cov': np.diag([1.,1.])**2}
+
+    def update_params(self, msg):
+        if msg['type'] == 'uniform':
+            self.type = 'uniform'
+            self.type_params = {'low': msg['low'], 'high': msg['high']}
+        elif msg['type'] == 'normal':
+            self.type = 'normal'
+            self.type_params = {'mean': msg['mean'], 'cov': np.diag(msg['std'])**2}
+        elif msg['type'] == 'lognormal':
+            self.type = 'lognormal'
+            self.type_params = {'mean': msg['mean'], 'std': msg['std']}
+        else:
+            raise ValueError('unknown type')            
+
+    def sample(self):
+        params = np.array([
+            [10, 1, 1, 3, 3, 3],
+            [10, 1, 1, 3, 3, 3],
+        ], dtype=np.float32)
+
+        if self.type == 'normal':
+            params[:, 0] = np.random.multivariate_normal(self.type_params['mean'], self.type_params['cov'])
+        elif self.type == 'lognormal':
+            params[0, 0] = np.random.lognormal(self.type_params['mean'][0], self.type_params['std'][0])
+            params[1, 0] = np.random.lognormal(self.type_params['mean'][1], self.type_params['std'][1])
+        else:
+            params[:, 0] = np.random.uniform(self.type_params['low'], self.type_params['high'], size=2)
+        
+        self.last_sample = params
+        return sshape.supercoords(params, shape=self.shape)
+
+
 def main():
     btargs, remainder = btb.parse_blendtorch_args()
 
-    SHAPE = (100,100)
-    PARAMS = np.array([
-        [10, 1, 1, 3, 3, 3],
-        [10, 1, 1, 3, 3, 3],
-    ], dtype=np.float32)
-    cur_params = None    
-    STDSCALE = 0.2
-    x,y,z = sshape.supercoords(sshape.FLOWER, shape=SHAPE)
+    configured = False
+    sampler = RandomSupershapeSampler()
+    x,y,z = sampler.sample()
     obj = sshape.make_bpy_mesh(x, y, z)
     mid = -1
 
     def pre_frame(duplex):
-        nonlocal mid,  cur_params
+        nonlocal mid, configured
         # Randomize cube rotation
         msg = duplex.recv(timeoutms=0)
         if msg != None:
-            PARAMS[0,0] = msg['m1']
-            PARAMS[1,0] = msg['m2']
+            sampler.update_params(msg)
             mid = msg['btmid']
-            print('cur params are now', PARAMS[:, 0])
+            configured = True
 
-        cur_params = PARAMS.copy()
-        cur_params[:, 0] += np.random.normal(scale=STDSCALE, size=2).astype(np.float32)
-        x,y,z = sshape.supercoords(cur_params, shape=SHAPE)
+        x,y,z = sampler.sample()
         sshape.update_bpy_mesh(x,y,z,obj)
         
     def post_frame(off, pub):
-        pub.publish(
-            image=off.render(), 
-            params=cur_params[:, 0],
-            mid=mid
-        )
+        if configured:
+            pub.publish(
+                image=off.render(), 
+                params=sampler.last_sample[:, 0],
+                mid=mid
+            )
 
     # Data source
     pub = btb.DataPublisher(btargs.btsockets['DATA'], btargs.btid)
-    duplex = btb.DuplexChannel(btargs.btsockets['CTRL'])
+    duplex = btb.DuplexChannel(btargs.btsockets['CTRL'], btargs.btid)
 
     # Setup default image rendering
     cam = btb.Camera()
-    off = btb.OffScreenRenderer(camera=cam, mode='rgb')
+    off = btb.OffScreenRenderer(camera=cam, mode='rgb', gamma_coeff=2.2)
     off.set_render_style(shading='SOLID', overlays=False)
 
     # Setup the animation and run endlessly
