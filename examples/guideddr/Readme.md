d892c01409a6c0b21b123b3b0c3eab562685b4ca
Heindl Christoph <cheind@profactor.at>
2020-08-17 09:42:56 +0200

    updated documentation

---
+++ examples/guideddr/Readme.md
@@ -0,0 +1,41 @@
+## Guided Domain Randomization
+
+This example demonstrates optimizing simulation parameters towards target distribution parameters by minimizing the expected loss between target and simulation images computed by a learnable discriminator function. We consider the target distribution parameters to be unknown, but assume access to a set of target images.
+
+In this example we rendered images of 3D parametric [supershapes](https://en.wikipedia.org/wiki/Superformula). A supershape can be described by 12 parameters and its appearance varies greatly along with the parameters (spheres, cubes, flower-like,...). In particular, the optimization tries to adjust the parameters 'm1' and 'm2' in such a way that the discriminator is not able to distinguish whether the given image is more likely to come from the simulation distribution or the target distribution. Once the discriminator reaches this state, the optimization parameters have usually converged towards the true target parameters.
+
+Note, we can frame this example in a GAN pattern: the generator consists of the simulation + probabilistic model governing the shape. The discriminator is a neural network attempting to distinguish between images of the target distribution and those to the simulation distribution. However, there is at least one **key difference** to GANs: the render function (Blender) is a black-box without access to gradients. We therefore frame the optimization as a minimization over an expected discriminator loss. 
+
+The image series below shows target images (left) and optimization progress at step (10,40,80).
+<p align="center">
+<img src="etc/real.png" width="200">
+<img src="etc/sim_samples_010.png" width="200">
+<img src="etc/sim_samples_040.png" width="200">
+<img src="etc/sim_samples_080.png" width="200">
+</p>
+
+### Run
+
+To recreate these results run [guideddr.py](./guideddr.py) as follows
+```
+python guideddr.py
+```
+which will generate output images in `./tmp/` and output the optimization progress on the console 
+```
+D step: mean real 0.7307174801826477 mean sim 0.6040560007095337
+D step: mean real 0.6737353205680847 mean sim 0.29735732078552246
+D step: mean real 0.825548529624939 mean sim 0.2806171178817749
+D step: mean real 0.8810040950775146 mean sim 0.1778283715248108
+S step: [1.25 2.95] [0.4205084443092346, 0.4205084443092346] mean sim 0.13082975149154663
+D step: mean real 0.9086431860923767 mean sim 0.21175092458724976
+S step: [1.2831218 2.9007795] [0.4404144883155823, 0.4418575167655945] mean sim 0.14274010062217712
+...
+```
+The true parameters being around 2.5/2.5 with a standard deviation of 0.1/0.1.
+
+### Dependencies
+
+Besides **blendtorch**, this examples requires [this tiny supershape](https://github.com/cheind/supershape) library to be acessible from within Blender (update `PYTHONPATH` prior executing the example).
+
+
+
--- examples/guideddr/guideddr.py
+++ examples/guideddr/guideddr.py
@@ -1,3 +1,6 @@
+'''Demonstrates adapting simulation parameters to match an empirical target distribution.
+'''
+
 from pathlib import Path
 import numpy as np
 import torch
@@ -9,15 +12,36 @@ import torchvision.utils as vutils
 
 from blendtorch import btt
 
+'''Batch size'''
 BATCH = 64
-REAL_LABEL = 1
+'''Target label at descriminator'''
+TARGET_LABEL = 1
+'''Simulation label at descriminator'''
 SIM_LABEL = 0
+'''Number of Blender instances.'''
 SIM_INSTANCES = 4
-
-MEAN_TRUE = 2.25 # rough sample range: 8.5-10.5
-STD_TRUE = 0.1 
+'''Long./Lat. log-normal supershape frequency (m1,m2) target mean'''
+MEAN_TARGET = 2.25 # rough sample range: 8.5-10.5
+'''Long./Lat. log-normal supershape frequency (m1,m2) target standard deviation'''
+STD_TARGET = 0.1 
 
 def generate_samples(n, mean, std):
+    '''Generate supershape examples via parameter sampling.
+    
+    We assume all parameter except for m1/m2 to be fixed in this
+    example. We consider mean/std parameters to be the parameters
+    of a log-normal distribution in order to avoid +/- parameter 
+    ambiguities that yield the same shape.
+
+    Params
+    ------
+    n: int
+        Number of supershape parameter samples to generate
+    mean: array
+        Log-normal frequency mean for m1/m2 parameter
+    std: array
+        Log-normal standard deviation for m1/m2 parameter
+    '''
     samples = torch.tensor([
         [0, 1, 1, 3, 3, 3],
         [0, 1, 1, 3, 3, 3],
@@ -29,23 +53,33 @@ def generate_samples(n, mean, std):
     return samples
 
 def update_simulations(remote_sims, n, mean, std):
+    '''Updates all remote simulations with new shape parameter samples.
+    
+    We split N samples into N//R chunks where R is the number of
+    simulation instances.
+    '''
     samples = generate_samples(n, mean, std)
     R = len(remote_sims)
     for remote, subset in zip(remote_sims, torch.chunk(samples, R)):
-        print(subset.shape)
         remote.send(shape_params=subset.numpy())
 
 def item_transform(item):
+    '''Transformation applied to each received simulation item.
+
+    Here we exctract the image, normalize it and return it together
+    with useful meta-data.
+    '''
     x = item['image'].astype(np.float32)
     x = (x - 127.5) / 127.5
     return np.transpose(x, (2, 0, 1)),  item['params'], item['btid']
 
-def get_real_images(dl, remotes, n=128):
+def get_target_images(dl, remotes, n=128):
+    '''Returns a set of images from the target distribution.'''
     update_simulations(
         remotes, 
         n, 
-        torch.tensor([MEAN_TRUE, MEAN_TRUE]), 
-        torch.tensor([STD_TRUE, STD_TRUE]))
+        torch.tensor([MEAN_TARGET, MEAN_TARGET]), 
+        torch.tensor([STD_TARGET, STD_TARGET]))
     images = []
     gen = iter(dl)
     for _ in range(n//BATCH):
@@ -54,11 +88,25 @@ def get_real_images(dl, remotes, n=128):
     return data.TensorDataset(torch.tensor(np.concatenate(images, 0)))
 
 def infinite_batch_generator(dl):
+    '''Generate infinite number of batches from a dataloader.'''
     while True:
         for data in dl:
             yield data
 
 class Discriminator(nn.Module):
+    '''Image descriminator.
+
+    The task of the discriminator is to distinguish images from the target
+    distribution from those of the simulator distribution. In the beginning
+    this is easy, as the target distribution is quite narrow, while the
+    simulator is producing images of supershapes from large spectrum. During
+    optimization of the simulation parameters the classification of images
+    will get continously harder as the simulation parameters are tuned
+    towards the (unkown) target distribution parameters.
+    
+    The discriminator weights are trained via backpropagation.
+    '''
+
     def __init__(self):
         super().__init__()
         ndf = 32
@@ -84,20 +132,23 @@ class Discriminator(nn.Module):
             nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
+        self.apply(self._weights_init)
+
+    def _weights_init(self, m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        elif classname.find('BatchNorm') != -1:
+            torch.nn.init.normal_(m.weight, 1.0, 0.02)
+            torch.nn.init.zeros_(m.bias)
 
     def forward(self, x):
         x = self.features(x)
         return x.view(-1, 1).squeeze(1)
 
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        torch.nn.init.normal_(m.weight, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        torch.nn.init.normal_(m.weight, 1.0, 0.02)
-        torch.nn.init.zeros_(m.bias)
 
 def log_probs(theta_mean, theta_std, samples):
+    '''Returns the log-probabilities of the given samples w.r.t the log-normal distributions.'''
     ln0 = LogNormal(theta_mean[0], torch.exp(theta_std[0]))
     ln1 = LogNormal(theta_mean[1], torch.exp(theta_std[1]))
     return (
@@ -117,51 +168,55 @@ def main():
         named_sockets=['DATA', 'CTRL'],
     )
 
+    # Create an untrained discriminator.
     dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     netD = Discriminator().to(dev)
-    netD.apply(weights_init)
 
     # Launch Blender
     with btt.BlenderLauncher(**launch_args) as bl:
-        # Create remote dataset and limit max length to 16 elements.
+        # Create remote dataset
         addr = bl.launch_info.addresses['DATA']
-        sim_ds = btt.RemoteIterableDataset(addr, max_items=100000, item_transform=item_transform)        
+        sim_ds = btt.RemoteIterableDataset(addr, item_transform=item_transform)        
         sim_dl = data.DataLoader(sim_ds, batch_size=BATCH, num_workers=0, shuffle=False)
 
+        # Create a control channel to each Blender instance. We use this channel to 
+        # communicate new shape parameters to be rendered.
         addr = bl.launch_info.addresses['CTRL']
         remotes = [btt.DuplexChannel(a) for a in addr]
 
-        real_ds = get_real_images(sim_dl, remotes, n=BATCH)
-        real_dl = data.DataLoader(real_ds, batch_size=BATCH, num_workers=0, shuffle=True)
+        # Fetch images of the target distribution. In the following we assume the 
+        # target distribution to be unknown.
+        target_ds = get_target_images(sim_dl, remotes, n=BATCH)
+        target_dl = data.DataLoader(target_ds, batch_size=BATCH, num_workers=0, shuffle=True)
        
-        # # Start solution
+        # Initial simulation parameters. The parameters in mean and std are off from the target
+        # distribution parameters. Note that we especially enlarge the scale of the distribution
+        # to get explorative behaviour in the beginning.
         theta_mean = torch.tensor([1.2, 3.0], requires_grad=True)
-        theta_std = torch.log(torch.tensor([STD_TRUE*8, STD_TRUE*8])).requires_grad_() # initial scale has to be larger the farther away we assume to be from solution.
-        
-        # # ok slow
-        # # optD = optim.Adam(netD.parameters(), lr=1e-5, betas=(0.5, 0.999))
-        # # optS = optim.Adam([sim_theta_mean, sim_theta_std], lr=5e-2, betas=(0.5, 0.999))
+        theta_std = torch.log(torch.tensor([STD_TARGET*4, STD_TARGET*4])).requires_grad_() # initial scale has to be larger the farther away we assume to be from solution.
 
-        # ok faster
+        # Setup discriminator and simulation optimizer
         optD = optim.Adam(netD.parameters(), lr=5e-5, betas=(0.5, 0.999))
         optS = optim.Adam([theta_mean, theta_std], lr=5e-2, betas=(0.7, 0.999))
 
-        gen_real = infinite_batch_generator(real_dl)
+        # Get generators for image batches from target and simulation.
+        gen_real = infinite_batch_generator(target_dl)
         gen_sim = infinite_batch_generator(sim_dl)
         crit = nn.BCELoss(reduction='none')
         
         epoch = 0
-        b = 0.
+        b = 0.          # baseline to reduce variance of gradient estimator.        
+        balpha = 0.95   # baseline exponential smoothing factor.
         first = True
-        balpha = 0.95
 
+        # Send instructions to render supershapes from the starting point.
         update_simulations(remotes, BATCH, theta_mean.detach(), torch.exp(theta_std.detach()))
         for (real, sim) in zip(gen_real, gen_sim):
-            
-            label = torch.full((BATCH,), REAL_LABEL, dtype=torch.float32, device=dev)
+            ### Train the discriminator from target and simulation images.
+            label = torch.full((BATCH,), TARGET_LABEL, dtype=torch.float32, device=dev)
             netD.zero_grad()
-            real_img = real[0].to(dev)
-            output = netD(real_img)
+            target_img = real[0].to(dev)
+            output = netD(target_img)
             errD_real = crit(output, label)
             errD_real.mean().backward()
             D_real = output.mean().item()
@@ -177,11 +232,21 @@ def main():
                 optD.step()
                 print('D step: mean real', D_real, 'mean sim', D_sim)
 
+            ### Optimize the simulation parameters.
+            # We update the simulation parameters once the discriminator
+            # has started to converge. Note that unlike to GANs the generator 
+            # (simulation) is giving meaningful output from the very beginning, so we
+            # give the discriminator some time to adjust and avoid spurious signals
+            # in gradient estimation of the simulation parameters.
+            #
+            # Note, the rendering function is considered a black-box and we cannot
+            # propagate through it. Therefore we reformulate the optimization as
+            # minimization of an expectation with the parameters in the distribution
+            # the expectation runs over. Using score-function gradients permits gradient
+            # based optimization _without_ access to gradients of the render function.
             if not first or (D_real - D_sim) > 0.7:
-                # Update sim params, called only every 20epochs or so.
-                # blender generates new data meanwhile with old MID.
                 optS.zero_grad()
-                label.fill_(REAL_LABEL)
+                label.fill_(TARGET_LABEL)
                 with torch.no_grad():
                     output = netD(sim_img)
                     errS_sim = crit(output, label)
@@ -200,11 +265,12 @@ def main():
                 print('S step:', theta_mean.detach().numpy(), torch.exp(theta_std).tolist(), 'mean sim', GD_sim)
                 first = False        
 
+            # Generate shapes according to updated parameters.
             update_simulations(remotes, BATCH, theta_mean.detach(), torch.exp(theta_std.detach()))
                 
             epoch += 1
             if epoch % 10 == 0:
-                vutils.save_image(real_img, 'tmp/real.png', normalize=True)
+                vutils.save_image(target_img, 'tmp/real.png', normalize=True)
                 vutils.save_image(sim_img, 'tmp/sim_samples_%03d.png' % (epoch), normalize=True)
                 vutils.save_image(sim_img[sim_btid==0], 'tmp/sim_samples_mask_%03d.png' % (epoch), normalize=True)
 
