fdca22a0fd54e673ba220926355849fa0f3f3896
Heindl Christoph <christoph.heindl@gmail.com>
2020-09-04 13:01:41 +0200

    switched to 2.9

--- etc/export_paths.bat
+++ etc/export_paths.bat
@@ -1,4 +1,3 @@
 @echo off
-set PATH=c:\Program Files\Blender Foundation\Blender 2.83;%PATH%
-set PYTHONPATH=%~dp0..\pkg_blender;%~dp0..\pkg_pytorch;%PYTHONPATH%
+set PATH=c:\Program Files\Blender Foundation\Blender 2.90;%PATH%
 @echo on
