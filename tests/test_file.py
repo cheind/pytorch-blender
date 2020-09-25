e39884177c83d7fc7003e767471f4c24c9605f79
Heindl Christoph <christoph.heindl@gmail.com>
2020-09-25 05:44:55 +0200

    updated tests

--- tests/test_file.py
+++ tests/test_file.py
@@ -11,3 +11,19 @@ def test_file_recorder_reader(tmp_path):
     assert len(r) == 7
     for i in range(7):
         assert r[i]['value'] == i
+
+@pytest.mark.background
+def test_file_recorder_reader_exception(tmp_path):
+    try:
+        with FileRecorder(outpath=tmp_path/'record.mpkl', max_messages=10, update_header=1) as rec:
+            rec.save({'value':0}, is_pickled=False)
+            rec.save({'value':1}, is_pickled=False)
+            rec.save({'value':2}, is_pickled=False)
+            raise ValueError('err')
+    except ValueError:
+        pass
+
+    r = FileReader(tmp_path/'record.mpkl')
+    assert len(r) == 3
+    for i in range(3):
+        assert r[i]['value'] == i
