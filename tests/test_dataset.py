6aeb5800c8fb3f0b4ddd33b8f12b069dcd652e9b
Heindl Christoph <christoph.heindl@gmail.com>
2020-09-30 12:51:30 +0200

    added test to cover case when blender exits

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,5 +1,6 @@
 import pytest
 from pathlib import Path
+import numpy as np
 from torch.utils.data import DataLoader
 
 from blendtorch import btt
@@ -32,3 +33,35 @@ def test_dataset():
 
 
         assert count == 4
+
+
+@pytest.mark.background
+def test_dataset_robustness():
+    launch_args = dict(
+        scene='',
+        script=BLENDDIR/'dataset_robust.blend.py', 
+        num_instances=2,        
+        named_sockets=['DATA'],
+        background=True,
+    )
+
+    with btt.BlenderLauncher(**launch_args) as bl:
+        addr = bl.launch_info.addresses['DATA']
+
+        # Note, https://github.com/pytorch/pytorch/issues/44108
+        ds = btt.RemoteIterableDataset(addr, max_items=128)
+        dl = DataLoader(ds, batch_size=4, num_workers=0, drop_last=False, shuffle=False)
+        
+
+        count = 0
+        ids = []
+        for item in dl:
+            assert item['img'].shape == (4,64,64)
+            assert item['frameid'].shape == (4,)
+            ids.append(item['btid'])
+            count += 1
+        assert count == 32
+        ids = np.concatenate(ids)
+        assert (ids==1).sum() == 1
+        assert (ids==0).sum() == 127
+        
