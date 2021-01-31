04244ea9cf20a3defbf2e6dea142d2dd6544f853
Christoph Heindl <christoph.heindl@gmail.com>
2021-01-31 14:25:38 +0100

    initial working version

--- examples/normals_depth/normals_depth.blend.py
+++ examples/normals_depth/normals_depth.blend.py
@@ -3,26 +3,40 @@ import numpy as np
 
 import blendtorch.btb as btb
 
+# Update python-path with current blend file directory,
+# so that package `tless` can be found.
+import sys
+p = bpy.path.abspath("//")
+if p not in sys.path:
+    sys.path.append(p)
+from normals_depth import scene
+
+SHAPE = (50,50)
+NSHAPES = 20
 
 def main():
+    def pre_anim(meshes):
+        # Called before each animation
+        # Randomize supershapes
+        [scene.update_mesh(m, sshape_res=SHAPE) for m in meshes]
+
+    def post_frame(render, pub, animation):
+        # After frame
+        if anim.frameid == 2:
+            imgs = render.render()
+            pub.publish(
+                normals=imgs['normals'],
+                depth=imgs['depth']
+            )
+
     # Parse script arguments passed via blendtorch launcher
     btargs, remainder = btb.parse_blendtorch_args()
 
+    # Fetch camera
     cam = bpy.context.scene.camera
 
-    def pre_frame():
-        # Randomize cube rotation
-        #cube.rotation_euler = np.random.uniform(0, np.pi, size=3)
-        pass
-
-    def post_frame(render, anim, cam):
-        # Called every after Blender finished processing a frame.
-        # Will be sent to one of the remote dataset listener connected.
-        imgs = render.render()
-        pub.publish(
-            normals=imgs['normals'],
-            depth=imgs['depth']
-        )
+    # Setup supershapes
+    meshes = scene.prepare(NSHAPES, sshape_res=SHAPE)
 
     # Data source
     pub = btb.DataPublisher(btargs.btsockets['DATA'], btargs.btid)
@@ -40,9 +54,9 @@ def main():
 
     # Setup the animation and run endlessly
     anim = btb.AnimationController()
-    anim.pre_frame.add(pre_frame)
-    anim.post_frame.add(post_frame, render, anim, cam)
-    anim.play(frame_range=(1, 2), num_episodes=-1, use_offline_render=False)
+    anim.pre_animation.add(pre_anim, meshes)
+    anim.post_frame.add(post_frame, render, pub, anim)
+    anim.play(frame_range=(1, 3), num_episodes=-1, use_offline_render=False)
 
 
 main()
--- examples/normals_depth/normals_depth/scene.py
+++ examples/normals_depth/normals_depth/scene.py
@@ -6,27 +6,28 @@ import supershape as sshape
 
 SCN = bpy.context.scene
 
+
 def create_sshape_mesh(shape, material=None, fake_user=True):
-    new_obj = sshape.make_bpy_mesh(shape, name='sshape', coll=False)
+    new_obj = sshape.make_bpy_mesh(shape, name='sshape', coll=False, weld=True)
     new_obj.data.use_fake_user = fake_user
     new_obj.use_fake_user = fake_user
     if material is not None:
-      new_obj.data.materials.append(material)
-      new_obj.active_material_index = 0
+        new_obj.data.materials.append(material)
+        new_obj.active_material_index = 0
     return new_obj
 
 
-def prepare(n_sshapes, sshape_res=(100,100), collection='Generated', mat='Normals', fake_user=False):
+def prepare(n_sshapes, sshape_res=(100, 100), collection='Generated', mat='Normals', fake_user=False):
     coll = SCN.collection.children[collection]
     mat = bpy.data.materials[mat]
 
     sshapes = [
-        create_sshape_mesh(sshape_res, material=mat, fake_user=fake_user) 
+        create_sshape_mesh(sshape_res, material=mat, fake_user=fake_user)
         for _ in range(n_sshapes)
     ]
 
     for s in sshapes:
-        coll.objects.link(s)        
+        coll.objects.link(s)
         SCN.rigidbody_world.collection.objects.link(s)
         # Rigid body settings
         s.rigid_body.enabled = True
@@ -38,14 +39,16 @@ def prepare(n_sshapes, sshape_res=(100,100), collection='Generated', mat='Normal
 
     return sshapes
 
-def update_mesh(mesh, sshape_res=(100,100)):
+
+def update_mesh(mesh, sshape_res=(100, 100)):
     params = np.random.uniform(
         low=[2.00, 1, 1, 8.0, 8.0, 8.0],
         high=[4.00, 1, 1, 10.0, 10.0, 10.0],
         size=(2, 6)
     )
     scale = np.random.uniform(0.02, 0.10, size=3)
-    x,y,z = sshape.supercoords(params, shape=sshape_res)    
+    x, y, z = sshape.supercoords(params, shape=sshape_res)
     sshape.update_bpy_mesh(x*scale[0], y*scale[1], z*scale[2], mesh)
-    mesh.location = np.random.uniform(low=[-0.5, -0.5, 1],high=[0.5,0.5,3],size=(3))
-    mesh.rotation_euler = np.random.uniform(low=-np.pi, high=np.pi,size=(3))
+    mesh.location = np.random.uniform(
+        low=[-0.5, -0.5, 1], high=[0.5, 0.5, 3], size=(3))
+    mesh.rotation_euler = np.random.uniform(low=-np.pi, high=np.pi, size=(3))
