1c8d24f7e96605bc5c83feb006c359fffe051049
Christoph Heindl <christoph.heindl@gmail.com>
2021-02-02 09:55:38 +0100

    version bump

--- pkg_blender/blendtorch/btb/__init__.py
+++ pkg_blender/blendtorch/btb/__init__.py
@@ -8,4 +8,4 @@ from .camera import Camera
 from .duplex import DuplexChannel
 from . import env, utils
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
--- pkg_pytorch/blendtorch/btt/__init__.py
+++ pkg_pytorch/blendtorch/btt/__init__.py
@@ -6,4 +6,4 @@ from .duplex import DuplexChannel
 from . import env
 from . import colors
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
