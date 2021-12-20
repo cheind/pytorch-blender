1bfa7045aeb225eba42868560b4d45070382c6e0
Christoph.Heindl <cheind@profactor.at>
2021-12-20 08:12:57 +0100

    linting

--- pkg_blender/blendtorch/btb/env.py
+++ pkg_blender/blendtorch/btb/env.py
@@ -1,6 +1,4 @@
-import bpy
 import zmq
-import sys
 import numpy as np
 
 from .animation import AnimationController
@@ -10,21 +8,21 @@ from .camera import Camera
 
 
 class BaseEnv:
-    '''Abstract base class for environments to be interacted with by agents.
+    """Abstract base class for environments to be interacted with by agents.
 
     This class is what `gym.Env` is to OpenAI gym: it defines the basic interface
     required to be implemented by all Blender environments.
 
-    Blender defines a callback based animation system. This also affects `BaseEnv` in that it 
+    Blender defines a callback based animation system. This also affects `BaseEnv` in that it
     requires the agent to be given by a callable method having following signature:
         cmd, action = agent(env, **kwargs)
     The arguments passed via **kwargs are at least `obs`, `reward`, `done`. Other variables
-    correspond to additional information (`info` dict in OpenAI). The agent is expected to 
-    return a command (BaseEnv.CMD_RESTART or BaseEnv.CMD_STEP) with an optional `action` 
-    to perform. Note, `action`, `obs`, `reward` and `info` depend on the actual environment 
+    correspond to additional information (`info` dict in OpenAI). The agent is expected to
+    return a command (BaseEnv.CMD_RESTART or BaseEnv.CMD_STEP) with an optional `action`
+    to perform. Note, `action`, `obs`, `reward` and `info` depend on the actual environment
     implementation.
 
-    Since a callback based agent is unusual to OpenAI users, blendtorch offers a 
+    Since a callback based agent is unusual to OpenAI users, blendtorch offers a
     `RemoteControlledAgent`, that communicates with a remotly implemented agent. The remote
     agent can then be implements with the common blocking: `agent.step()`, `agent.restart()`.
     See `blendtorch.btt` for details.
@@ -34,7 +32,7 @@ class BaseEnv:
      - `BaseEnv._env_prepare_step(action) to apply an action in a pre-frame manner.
      - `BaseEnv._env_post_step()` to gather the environment state, reward and other variables
         after the frame has completed (i.e after physics, and animation have computed their values).
-    '''
+    """
 
     STATE_INIT = object()
     STATE_RUN = object()
@@ -42,7 +40,7 @@ class BaseEnv:
     CMD_STEP = object()
 
     def __init__(self, agent):
-        '''Initialize the environment.'''
+        """Initialize the environment."""
         self.events = AnimationController()
         self.events.pre_frame.add(self._pre_frame)
         self.events.pre_animation.add(self._pre_animation)
@@ -55,7 +53,7 @@ class BaseEnv:
         self.state = BaseEnv.STATE_INIT
 
     def run(self, frame_range=None, use_animation=True):
-        '''Run the environment.
+        """Run the environment.
 
         This hooks with animation system to receive callbacks. The callbacks eventually
         will invoke the actual environments interface methods as described above.
@@ -70,17 +68,18 @@ class BaseEnv:
             variant. Set this to True, when you want to see the agents actions rendered
             live in Blender. When set to False, does not allow Blender UI to refresh, but
             may run at much higher FPS. Consider when training the agent.
-        '''
+        """
         self.frame_range = AnimationController.setup_frame_range(frame_range)
         self.events.play(
             # we allow playing the simulation past end.
             (self.frame_range[0], 2147483647),
             num_episodes=-1,
             use_animation=use_animation,
-            use_offline_render=True)
+            use_offline_render=True,
+        )
 
     def attach_default_renderer(self, every_nth=1):
-        '''Attach a default renderer to the environment.
+        """Attach a default renderer to the environment.
 
         Convenience function to provide render images for remotely controlled agents (i.e `env.render()`). Uses the default camera perspective for image generation.
 
@@ -90,66 +89,63 @@ class BaseEnv:
         ------
         every_nth: int
             Render every nth frame of the simulation.
-        '''
+        """
 
-        self.renderer = OffScreenRenderer(camera=Camera(), mode='rgb')
+        self.renderer = OffScreenRenderer(camera=Camera(), mode="rgb")
         self.render_every = every_nth
 
     def _pre_frame(self):
-        '''Internal pre-frame callback.'''
-        self.ctx['time'] = self.events.frameid
-        self.ctx['done'] |= (self.events.frameid >= self.frame_range[1])
+        """Internal pre-frame callback."""
+        self.ctx["time"] = self.events.frameid
+        self.ctx["done"] |= self.events.frameid >= self.frame_range[1]
         if self.events.frameid > self.frame_range[0]:
             cmd, action = self.agent(self, **self.ctx)
             if cmd == BaseEnv.CMD_RESTART:
                 self._restart()
             elif cmd == BaseEnv.CMD_STEP:
-                if np.all(action != None):
+                if np.all(action is not None):
                     self._env_prepare_step(action)
-                    self.ctx['prev_action'] = action
+                    self.ctx["prev_action"] = action
                 self.state = BaseEnv.STATE_RUN
 
     def _pre_animation(self):
-        '''Internal pre-animation callback.'''
+        """Internal pre-animation callback."""
         self.state = BaseEnv.STATE_INIT
-        self.ctx = {'prev_action': None, 'done': False}
+        self.ctx = {"prev_action": None, "done": False}
         init_ctx = self._env_reset()
         if init_ctx is not None:
             self.ctx = {**self.ctx, **init_ctx}
 
     def _post_frame(self):
-        '''Internal post-frame callback.'''
+        """Internal post-frame callback."""
         self._render(self.ctx)
         next_ctx = self._env_post_step()
         self.ctx = {**self.ctx, **next_ctx}
 
     def _render(self, ctx):
-        '''Internal render command.'''
+        """Internal render command."""
         cur, start = self.events.frameid, self.frame_range[0]
-        render = bool(
-            self.renderer and
-            ((cur - start) % self.render_every) == 0
-        )
+        render = bool(self.renderer and ((cur - start) % self.render_every) == 0)
         if render:
-            ctx['rgb_array'] = self.renderer.render()
+            ctx["rgb_array"] = self.renderer.render()
 
     def _restart(self):
-        '''Restart the environment internally.'''
+        """Restart the environment internally."""
         self.events.rewind()
 
     def _env_reset(self):
-        '''Reset the environment state. 
+        """Reset the environment state.
 
         To be implemented by actual environments. Returns nothing.
-        '''
+        """
         raise NotImplementedError()
 
     def _env_prepare_step(self, action):
-        '''Prepare environment with action.
+        """Prepare environment with action.
 
         Due to the callback system in  Blender, the agents `step` method
         is best split into two distinct function. One that takes the action
-        before a frame is animated/rendered and one that collects the new 
+        before a frame is animated/rendered and one that collects the new
         state/reward once the frame has completed. Doing so, allows
         the physics system to take the action before the frame simulation into
         consideration and thus work out the phyisical state at the end of frame.
@@ -157,16 +153,16 @@ class BaseEnv:
         In case a remote controlled agent is used, make sure the action is pickle-able.
 
         Returns nothing.
-        '''
+        """
 
         raise NotImplementedError()
 
     def _env_post_step(self):
-        '''Return the environments new state as dict.
+        """Return the environments new state as dict.
 
         Requires at least the following fields to be set: `obs`, `reward`. You might
-        also want to specify `done`. All other fields set will be passed to the agent 
-        as well. 
+        also want to specify `done`. All other fields set will be passed to the agent
+        as well.
 
         In case a remote controlled agent is used, make sure all items are pickle-able.
 
@@ -175,14 +171,14 @@ class BaseEnv:
         ddict: dict
             dictionary of key-values describing the new environment state as well as
             any reward and auxilary information.
-        '''
+        """
         raise NotImplementedError()
 
 
 class RemoteControlledAgent:
-    '''Agent implementation that receives commands from a remote peer.
+    """Agent implementation that receives commands from a remote peer.
 
-    Uses a request(remote-agent)/reply(self) pattern to model a [blocking] 
+    Uses a request(remote-agent)/reply(self) pattern to model a [blocking]
     service call. The agent is expected to initiate a request using a dictionary:
      - `cmd` field set either to `'reset'` or `'step'`.
      - `action` field set when `cmd=='step'`.
@@ -200,17 +196,18 @@ class RemoteControlledAgent:
     address: str
         ZMQ remote address to bind to.
     real_time: bool
-        Whether or not to continue simulation while waiting for a 
+        Whether or not to continue simulation while waiting for a
         new remote agent request. Default False.
     timeoutms: int
         Default timeout in milliseconds to wait for new agent requests,
         only applies when `real_time=True`.
-    '''
+    """
+
     STATE_REQ = 0
     STATE_REP = 1
 
     def __init__(self, address, real_time=False, timeoutms=DEFAULT_TIMEOUTMS):
-        '''Initialize the remote controlled agent.'''
+        """Initialize the remote controlled agent."""
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REP)
         self.socket.setsockopt(zmq.LINGER, 0)
@@ -221,7 +218,7 @@ class RemoteControlledAgent:
         self.state = RemoteControlledAgent.STATE_REQ
 
     def __call__(self, env, **ctx):
-        '''Process agent environment callback.'''
+        """Process agent environment callback."""
         flags = 0
         if self.real_time and (env.state == BaseEnv.STATE_RUN):
             flags = zmq.NOBLOCK
@@ -232,24 +229,24 @@ class RemoteControlledAgent:
                 self.state = RemoteControlledAgent.STATE_REQ
             except zmq.error.Again:
                 if not self.real_time:
-                    raise ValueError('Failed to send to remote agent.')
+                    raise ValueError("Failed to send to remote agent.")
                 return BaseEnv.CMD_STEP, None
 
         if self.state == RemoteControlledAgent.STATE_REQ:
             try:
                 rcv = self.socket.recv_pyobj(flags=flags)
-                assert rcv['cmd'] in ['reset', 'step']
+                assert rcv["cmd"] in ["reset", "step"]
                 self.state = RemoteControlledAgent.STATE_REP
 
-                if rcv['cmd'] == 'reset':
+                if rcv["cmd"] == "reset":
                     cmd = BaseEnv.CMD_RESTART
                     action = None
                     if env.state == BaseEnv.STATE_INIT:
                         # Already reset
                         cmd, action = self.__call__(env, **ctx)
-                elif rcv['cmd'] == 'step':
+                elif rcv["cmd"] == "step":
                     cmd = BaseEnv.CMD_STEP
-                    action = rcv['action']
+                    action = rcv["action"]
                 return cmd, action
             except zmq.error.Again:
                 return BaseEnv.CMD_STEP, None
--- tests/blender/env.blend.py
+++ tests/blender/env.blend.py
@@ -1,20 +1,19 @@
 import bpy
-from mathutils import Matrix
-import numpy as np
 
 from blendtorch import btb
+
 
 class MyEnv(btb.env.BaseEnv):
     def __init__(self, agent, done_after=10):
         super().__init__(agent)
-        self.cube = bpy.data.objects['Cube']
+        self.cube = bpy.data.objects["Cube"]
         self.count = 0
-        self.done_after=done_after
+        self.done_after = done_after
 
     def _env_reset(self):
-        self.cube.rotation_euler[2] = 0.
+        self.cube.rotation_euler[2] = 0.0
         self.count = 0
-        return dict(obs=self.cube.rotation_euler[2],count=self.count,done=False)
+        return dict(obs=self.cube.rotation_euler[2], count=self.count, done=False)
 
     def _env_prepare_step(self, action):
         self.cube.rotation_euler[2] = action
@@ -24,26 +23,25 @@ class MyEnv(btb.env.BaseEnv):
         angle = self.cube.rotation_euler[2]
         return dict(
             obs=angle,
-            reward=1. if abs(angle) > 0.5 else 0.,
+            reward=1.0 if abs(angle) > 0.5 else 0.0,
             done=self.events.frameid > self.done_after,
-            count=self.count
+            count=self.count,
         )
 
-def main():    
+
+def main():
     args, remainder = btb.parse_blendtorch_args()
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--done-after', default=10, type=int)
+    parser.add_argument("--done-after", default=10, type=int)
     envargs = parser.parse_args(remainder)
 
-    agent = btb.env.RemoteControlledAgent(
-        args.btsockets['GYM']        
-    )
+    agent = btb.env.RemoteControlledAgent(args.btsockets["GYM"])
     env = MyEnv(agent, done_after=envargs.done_after)
     if not bpy.app.background:
         env.attach_default_renderer(every_nth=1)
-    env.run(frame_range=(1,10000), use_animation=not bpy.app.background)
+    env.run(frame_range=(1, 10000), use_animation=not bpy.app.background)
+
 
 main()
-
