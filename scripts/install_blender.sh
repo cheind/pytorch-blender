171ced4223edfb89c9889b0a2c889d0112729069
Christoph Heindl <christoph.heindl@gmail.com>
2023-10-28 05:42:27 +0200

    updated blender

--- scripts/install_blender.sh
+++ scripts/install_blender.sh
@@ -1,10 +1,10 @@
 set -e
 
-NAME="blender-3.1.2-linux-x64"
+NAME="blender-3.6.5-linux-x64"
 NAMETAR="${NAME}.tar.xz"
 CACHE="${HOME}/.blender-cache"
 TAR="${CACHE}/${NAMETAR}"
-URL="https://mirror.clarkson.edu/blender/release/Blender3.1/${NAMETAR}"
+URL="https://mirror.clarkson.edu/blender/release/Blender3.6/${NAMETAR}"
 
 echo "Installing Blender ${NAME}"
 mkdir -p $CACHE
